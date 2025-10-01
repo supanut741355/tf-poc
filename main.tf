9fafc2b217904aae0a76e424cc0ae99b04053511
supanut741355 <supanut.wizard@gmail.com>
2025-10-01 16:27:07 +0700

    add: vpc and vm

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "myorder-beta"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
